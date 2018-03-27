07b12f3db5636489199c504c8451a32974d9761c
Takuma Homma <nagomimatcha@gmail.com>
2018-03-27 22:10:19 +0900

    Add spec~

--- spec/local_rules_spec.rb
+++ spec/local_rules_spec.rb
@@ -1,41 +1,55 @@
-require File.expand_path("../spec_helper", __FILE__)
+require File.expand_path('../spec_helper', __FILE__)
 
 module Danger
   describe Danger::DangerLocalRules do
-    it "should be a plugin" do
+    let(:dangerfile) { testing_dangerfile }
+    let(:plugin) { dangerfile.local_rules }
+    let(:pr_diff) { dummy_pr_diff }
+    let(:load_data) do
+      {
+        "failure" => {"legacy"=>"No!", "typo"=>"type"}, 
+        "warning" => {"TODO"=>"You should check"}
+      }
+    end
+
+    it 'should be a plugin' do
       expect(Danger::DangerLocalRules.new(nil)).to be_a Danger::Plugin
     end
 
-    #
-    # You should test your custom attributes and methods here
-    #
-    describe "with Dangerfile" do
+    describe '#check' do
+      let(:github_plugin)  { Danger::DangerfileGitHubPlugin }
+
       before do
-        @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.local_rules
+        allow(YAML).to receive(:load_file).with('.danger_local_rules.yml').and_return(load_data)
+        allow_any_instance_of(github_plugin).to receive(:pr_diff).and_return(pr_diff)
       end
 
-      # Some examples for writing tests
-      # You should replace these with your own.
-
-      it "Warns on a monday" do
-        monday_date = Date.parse("2016-07-11")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
+      context 'passed' do
+        it do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to eq(0)
+        end
       end
 
-      it "Does nothing on a tuesday" do
-        monday_date = Date.parse("2016-07-12")
-        allow(Date).to receive(:today).and_return monday_date
+      context 'diff which included string to warn' do
+        let(:pr_diff) { dummy_warning_pr_diff }
 
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq([])
+        it 'should have a warning comment' do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to eq(0)
+          expect(dangerfile.status_report[:warnings].length).to eq(1)
+        end
       end
 
+      context 'diff which included 2 strings to fail' do
+        let(:pr_diff) { dummy_failure_pr_diff }
+
+        it 'should have 2 failure comments' do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to eq(2)
+          expect(dangerfile.status_report[:warnings].length).to eq(0)
+        end
+      end
     end
   end
 end
